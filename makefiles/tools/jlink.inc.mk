a90983d73288aab74e5d07ffe6bfb31cf17ec4cd
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2019-05-06 12:05:55 +0200

    makefiles/tools/jlink.inc.mk: do not overwrite FLASHER/...
    Do not overwrite the already configured FLASHER/DEBUGGER/RESET
    variables.
    
    This will allow using it as debugger for boards using `edbg` like with
    `openocd.inc.mk`. It also allows setting FLASHER differently.

--- makefiles/tools/jlink.inc.mk
+++ makefiles/tools/jlink.inc.mk
@@ -1,7 +1,7 @@
-export FLASHER = $(RIOTTOOLS)/jlink/jlink.sh
-export DEBUGGER = $(RIOTTOOLS)/jlink/jlink.sh
-export DEBUGSERVER = $(RIOTTOOLS)/jlink/jlink.sh
-export RESET = $(RIOTTOOLS)/jlink/jlink.sh
+export FLASHER ?= $(RIOTTOOLS)/jlink/jlink.sh
+export DEBUGGER ?= $(RIOTTOOLS)/jlink/jlink.sh
+export DEBUGSERVER ?= $(RIOTTOOLS)/jlink/jlink.sh
+export RESET ?= $(RIOTTOOLS)/jlink/jlink.sh
 
 FLASHFILE ?= $(BINFILE)
 
