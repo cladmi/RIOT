bcfc056f1b9c33a39e5b887081fb0eab5bcb3b37
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2019-09-25 15:35:32 +0200

    tests: add an automated manual test script that saves the output

--- tests/pthread_shelltest/tests/01-run.py
+++ tests/pthread_shelltest/tests/01-run.py
@@ -1,14 +1,38 @@
 #!/usr/bin/env python3
 
+import os
 import sys
+import time
+import pexpect
 from testrunner import run
 
 
 def testfunc(child):
-    child.expect_exact(u'[START] Spawning threads')
-    child.expect(r'\.+')
-    child.expect("[SUCCESS]")
-    child.expect("test end")
+    # Try to wait for the shell
+    for _ in range(0, 10):
+        child.sendline("help")
+        if child.expect_exact(["Command", pexpect.TIMEOUT], timeout=1) == 0:
+            break
+    child.expect('> ')
+
+    child.sendline('create')
+    child.expect('> ')
+
+    # Add delay to be sure 'idle' ran or anything.
+    time.sleep(1)
+    child.sendline('ps')
+    child.expect('> ')
+    with open('ps_output.%s.before' % os.environ['BOARD'], 'w+') as ps_output:
+        ps_output.write(child.before)
+
+    child.sendline('kill 1')
+    child.expect('> ')
+
+    time.sleep(1)
+    child.sendline('ps')
+    child.expect('> ')
+    with open('ps_output.%s.after' % os.environ['BOARD'], 'w+') as ps_output:
+        ps_output.write(child.before)
 
 
 if __name__ == "__main__":
