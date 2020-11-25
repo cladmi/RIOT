4bfab1bb44af199b9d054854d90d4b179d72bc2b
cladmi <hartergaetan@gmail.com>
2020-11-25 15:04:02 +0100

    pkg/wolfmqtt: first version of generated options for RIOT
    This is the version of './configure' genererated file adapted to RIOT
    variables, options, things.

---
+++ pkg/wolfmqtt/include/options.h
@@ -0,0 +1,33 @@
+/* wolfmqtt options.h
+ * generated from configure options
+ *
+ * Copyright (C) 2006-2020 wolfSSL Inc.
+ *
+ * * This file is part of wolfMQTT.
+ *
+ * It has been modified to handle configuration through RIOT build variables
+ *
+ */
+
+#ifndef WOLFMQTT_OPTIONS_H
+#define WOLFMQTT_OPTIONS_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#undef  SIZEOF_LONG
+#define SIZEOF_LONG sizeof(long)
+
+#undef  WOLFMQTT_DISCONNECT_CB
+#define WOLFMQTT_DISCONNECT_CB
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* WOLFMQTT_OPTIONS_H */
+
